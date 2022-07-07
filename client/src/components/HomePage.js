7e64bdf781ce946135ade192cad4077f3d16ff35
Maryam Mohsenpour <58438858+bita2019@users.noreply.github.com>
2022-07-07 14:50:34 +0100

    MUI not working

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./Style.css";
 import myData from "./data.json";
-import { Button } from "@material-ui/core";
 
 function HomePage() {
   return (
     <div className="root">
       <h1>This is the home page</h1>
       <input type="text" placeholder="Search ... " />
-      <Button color="primary">Search</Button>
+      <button color="blue">Search</button>
+
       <div>
         <div class="dropdown">
           <button onclick="myFunction()" class="dropbtn">
@@ -50,5 +50,37 @@ function HomePage() {
     </div>
   );
 }
+//search function :
+// function setup() {
+
+//   searchBox.addEventListener("keyup", onSearchKeyUp);
+//   fetch("https://api.tvmaze.com/shows/82/episodes")
+//     .then((response) => response.json())
+//     .then((data) => {
+//       console.log("This is data", data);
+//       makePageForEpisodes(data);
+//     });
+// }
+//function onSearchKeyUp(event) {
+// const searchTerm = event.target.value.toLowerCase();
+// const allEpisodes = getLiveEpisodes();
+
+//   const filteredEpisodes = allEpisodes.filter((e) => {
+//     const episodeName = e.name.toLowerCase();
+//     const episodeSummary = e.summary.toLowerCase();
+
+//     return (
+//       episodeName.includes(searchTerm) || episodeSummary.includes(searchTerm)
+//     );
+//   });
+
+//   const filteredCount = filteredEpisodes.length;
+//   const allCount = allEpisodes.length;
+
+//   const countString = `Displaying ${filteredCount} / ${allCount}`;
+
+//   searchCount.innerText = countString;
+//   makePageForEpisodes(filteredEpisodes);
+// }
 
 export default HomePage;
