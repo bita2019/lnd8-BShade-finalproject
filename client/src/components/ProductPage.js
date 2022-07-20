65a4198a90dd0b8b915dc5d96b7d0f232ddd69aa
Maryam Mohsenpour <58438858+bita2019@users.noreply.github.com>
2022-07-20 22:22:46 +0100

    image url taken

--- client/src/components/ProductPage.js
+++ client/src/components/ProductPage.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Searchbar from "./Searchbar";
 import {
@@ -10,15 +10,13 @@ import {
   Typography,
 } from "@mui/material";
 
-
-function ProductPage({ allProducts}) {
+function ProductPage({ allProducts }) {
   // eslint-disable-next-line no-unused-vars
   const [items, setItems] = useState(allProducts);
- 
 
-//function ProductPage({ handlesearch }) {
- // const [items, setItems] = useState([]);
- // useEffect(() => {
+  //function ProductPage({ handlesearch }) {
+  // const [items, setItems] = useState([]);
+  // useEffect(() => {
   //  fetchItems();
   //}, []);
 
@@ -35,61 +33,62 @@ function ProductPage({ allProducts}) {
           item.description.toLowerCase().includes(searchInput.toLowerCase()) ||
           // item.information.toLowerCase().includes(searchInput.toLowerCase()) ||
           item.country.toLowerCase().includes(searchInput.toLowerCase())
-    );
+      );
   const styles = {
     media: {
-      display: 'block',
-      maxWidth: '20ch',
-      maxHeight: '100%',
-      width: 'auto',
-      height: 'auto',
-      marginLeft: '5rem'
-    }
+      display: "block",
+      maxWidth: "20ch",
+      maxHeight: "100%",
+      width: "auto",
+      height: "auto",
+      marginLeft: "5rem",
+    },
   };
 
   return (
     <>
-    <Searchbar handlesearch={handlesearch} />
+      <Searchbar handlesearch={handlesearch} />
       <Box sx={{ flexGrow: 1, mt: 5 }}>
-      <Grid
-        container
-        spacing={{ xs: 2, md: 3 }}
-        columns={{ xs: 1, sm: 8, md: 12 }}
-      >
-        {filtered.map((item, index) => (
-          <Grid item xs={2} sm={4} md={4} key={index}>
-            <Card sx={{ maxWidth: 400 }}>
-              <Link to={`/productpage/${item.id}`}>
-
-                <CardMedia
-                  component="img"
-                  image={item.image}
-                  alt={item.name}
-                  style={styles.media}
-                />
-
-              </Link>
-              <CardContent>
-                <Link
-                  to={`/productpage/${item.id}`}
-                  style={{ textDecoration: "none" }}
-                >
-                  <Typography gutterBottom variant="h6" component="div">
-                    {item.name}
-                  </Typography>
+        <Grid
+          container
+          spacing={{ xs: 2, md: 3 }}
+          columns={{ xs: 1, sm: 8, md: 12 }}
+        >
+          {filtered.map((item, index) => (
+            <Grid item xs={2} sm={4} md={4} key={index}>
+              <Card sx={{ maxWidth: 400 }}>
+                <Link to={`/productpage/${item.id}`}>
+                  <CardMedia
+                    component="img"
+                    image={item.image}
+                    alt={item.name}
+                    style={styles.media}
+                  />
                 </Link>
-                <Typography variant="body2" color="text.secondary"></Typography>
-              </CardContent>
-              {/* <CardActions>
+                <CardContent>
+                  <Link
+                    to={`/productpage/${item.id}`}
+                    style={{ textDecoration: "none" }}
+                  >
+                    <Typography gutterBottom variant="h6" component="div">
+                      {item.name}
+                    </Typography>
+                  </Link>
+                  <Typography
+                    variant="body2"
+                    color="text.secondary"
+                  ></Typography>
+                </CardContent>
+                {/* <CardActions>
                 <Button size="small">Add to cart</Button>
                 <Button size="small">Learn More</Button>
               </CardActions> */}
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
       </Box>
-      </>
+    </>
   );
 }
 
