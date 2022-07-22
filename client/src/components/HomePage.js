ed5ed4f045dba973f7c1c95e25ef9d88b9384c6e
Maryam Mohsenpour <58438858+bita2019@users.noreply.github.com>
2022-07-22 14:20:06 +0100

    env added again

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,10 +1,11 @@
 import React from "react";
 import SellerProfile from "./SellerProfile";
+
 function HomePage() {
   return (
     <div>
       <h1>Welcome To Hujreh</h1>
-      {/* <SellerProfile /> */}
+      <SellerProfile />
     </div>
   );
 }
--- client/src/components/ProductsForSeller.js
+++ client/src/components/ProductsForSeller.js
@@ -1,13 +1,14 @@
+import React from "react";
 
-import React from "react"
-
+const BASEURL = "http://sellers/:id/products";
 
 function ProductsForSeller(props) {
-    return (
-        <div>
-            <h1>All products for seller</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>All products for seller</h1>
+      {/* GET endpoint /sellers/:id/products   which returns all products for the seller id  */}
+    </div>
+  );
 }
 
 export default ProductsForSeller;
