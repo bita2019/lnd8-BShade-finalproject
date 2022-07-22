c1eec7ab03cb40d001bca73d1e743b98fd2d0272
Maryam Mohsenpour <58438858+bita2019@users.noreply.github.com>
2022-07-22 10:49:51 +0100

    Push my Seller Profile changes before pull from Ramla endpoint

--- client/src/components/SellerProfile.js
+++ client/src/components/SellerProfile.js
@@ -1,47 +1,69 @@
 // import React , {useEffect,useState} from 'react';
 
-import * as React from "react";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
+import React from "react";
+import { styled } from "@mui/material/styles";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import { Stack } from "@mui/material";
+import { Box } from "@mui/material/Box";
 
-const SellerProfile = () => {
+const Img = styled("img")({
+  margin: "auto",
+  display: "block",
+  maxWidth: "100%",
+  maxHeight: "100%",
+});
+
+export default function SellerProfile() {
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        component="img"
-        height="140"
-        image="/static/images/cards/contemplative-reptile.jpg"
-        alt="green iguana"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Lizard
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Quantity</Button>
-      </CardActions>
-    </Card>
+    <>
+      <h3>Seller Profile :</h3>
+      <Paper
+        sx={{
+          p: 2,
+          margin: "auto",
+          maxWidth: 500,
+          flexGrow: 1,
+          backgroundColor: (theme) =>
+            theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+        }}
+      >
+        <Grid container spacing={2}>
+          <Grid item>
+            <Button sx={{ width: 128, height: 128 }}>
+              <Img alt="complex" src="/static/images/grid/complex.jpg" />
+            </Button>
+          </Grid>
+          <Grid item xs={12} sm container>
+            <Grid item xs container direction="column" spacing={2}>
+              <Grid item xs>
+                <Typography gutterBottom variant="subtitle1" component="div">
+                  Product Name
+                </Typography>
+                <Typography variant="body2" gutterBottom>
+                  {/* Full resolution 1920x1080 • JPEG */}
+                  Description
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  ID: 1030114
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography sx={{ cursor: "pointer" }} variant="body2">
+                  Remove
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid item>
+              <Typography variant="subtitle1" component="div">
+                Price : £1.00
+              </Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Paper>
+    </>
   );
-};
-// const fetch sellerItems = async (id)=>{
-//     const data = await fetch(`http://localhost:4444//sellers/:id/products${id}`);
-//     console.log(data);
-//     const item = await data.json();
-//     console.log(item);
-//     setItem(item);
-//   };
-
-// const SellerProfile = () => {
-
-// }
-
-export default SellerProfile;
+}
